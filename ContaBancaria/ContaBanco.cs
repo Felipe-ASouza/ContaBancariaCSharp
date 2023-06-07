2360173562540bf276d35c81f3ebe68de47c5370
Felipe Alexandre <felipe.alexandre3388@gmail.com>
2023-06-07 00:11:44 -0300

    Primeira versao conta bancaria com Csharp

---
+++ ContaBancaria/ContaBanco.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace ContaBancaria
+{
+    public class ContaBanco
+    {
+        public int NumeroDaConta { get; private set; }
+        public string TitularDaConta { get; set; }
+        public double Saldo { get; private set; }
+
+
+        public ContaBanco(int numero, string titular)
+        {
+            NumeroDaConta = numero;
+            TitularDaConta = titular;
+        }
+
+        public ContaBanco(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + NumeroDaConta + ", Titular: " + TitularDaConta + ", Saldo: $ " + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ContaBanco banco;
+            
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if(resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre com o depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                banco = new ContaBanco(numero, titular, depositoInicial);
+            }
+            else
+            {
+                banco = new ContaBanco(numero, titular);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(banco);
+        }
+    }
+}
